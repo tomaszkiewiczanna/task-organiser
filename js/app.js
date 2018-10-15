5acc9d2ba3cf284d38b5d7d2bb25a8293f481247
Anna Tomaszkiewicz <tomaszkiewicz.ania@gmail.com>
2018-10-15 13:41:04 +0200

    code refactoring

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', ()=>{
     //zamienne
     const infoIn = document.querySelector('.infoIn');
     const addButton = document.querySelector('#infoIn-addTaskButton');
@@ -9,6 +9,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     const infoOut = document.querySelector('.infoOut');
     const taskListUl = document.querySelector('#infoOut-taskList');
+
 
     const monthNames = [
         "January", "February", "March",
@@ -28,7 +29,7 @@ document.addEventListener('DOMContentLoaded', function () {
     infoOut.insertBefore(counterSpan, infoOut.firstChild);
     counterSpan.innerHTML = `You have <span id="task-counter-numb">0</span> tasks to make.`;
 
-
+    //FUNKCJE
     function refreshCounter() {
         finishedTasksArr = [];
         taskArr.forEach((task, i) => {
@@ -39,12 +40,29 @@ document.addEventListener('DOMContentLoaded', function () {
         document.querySelector('#task-counter-numb').innerHTML = taskArr.length - finishedTasksArr.length;
     }
 
-    //event start
-    addButton.addEventListener('click', function () {
+    // tworzenie li z tablicy zadań
+    function refreshList(arr) {
+        taskListUl.innerHTML = '';
+        arr.map(el => {
+            const newTaskLi = document.createElement('li');
+            newTaskLi.innerHTML = `
+                    <span class="infoOut-taskList-li-square"></span><h1>${el.taskName}</h1><br>
+                    <p>until ${el.taskDate.getDate()} ${monthNames[el.taskDate.getMonth()]} 
+                    ${el.taskDate.getFullYear()}</p><button class="deleteButton">Delete</button>
+                    <button class="completeButton">Completed</button>`;
+            taskListUl.appendChild(newTaskLi);
+            newTaskLi.id = el.taskKey;
+            newTaskLi.querySelector('.infoOut-taskList-li-square').style.background = colorsArr[el.taskPriority - 1];
+            if (el.taskDone) {newTaskLi.classList.add('completed')};
+        })
+    }
+
+    //dodanie zadania
+    addButton.addEventListener('click', () => {
         if (taskInput.value.length < 1 || taskInput.value.length >= 101) {
             alert('Task description must be between 1 - 100 letters!');
         } else if (priorityInput.value === "") {
-            alert('Set the priority form 1 up to 10 ');
+            alert('Set the priority from 1 up to 10 ');
         }
         else {
             //obiekt zadania dodany do tablicy
@@ -61,80 +79,55 @@ document.addEventListener('DOMContentLoaded', function () {
             priorityInput.value = "";
             dateInput = new Date();
             refreshCounter();
-
-            // tworzenie li z tablicy zadań
-            function refreshList(arr) {
-                taskListUl.innerHTML = '';
-                arr.map(el => {
-                    const newTaskLi = document.createElement('li');
-                    newTaskLi.innerHTML = `
-                    <span class="infoOut-taskList-li-square"></span><h1>${el.taskName}</h1><br>
-                    <p>until ${el.taskDate.getDate()} ${monthNames[el.taskDate.getMonth()]} 
-                    ${el.taskDate.getFullYear()}</p><button class="deleteButton">Delete</button>
-                    <button class="completeButton">Completed</button>
-                    `;
-                    taskListUl.appendChild(newTaskLi);
-                    newTaskLi.id = el.taskKey;
-                    newTaskLi.querySelector('.infoOut-taskList-li-square').style.background = colorsArr[el.taskPriority - 1];
-                    if (el.taskDone) {
-                        newTaskLi.classList.add('completed')
-                    }
-                    ;
-                })
-            }
             refreshList(taskArr);
-
-            //usuwanie zadania
-            const deleteButtons = taskListUl.querySelectorAll(".deleteButton");
-            deleteButtons.forEach(delBtn => {
-                delBtn.addEventListener('click', () => {
-                    let indexDelete;
-                    const parentLi = delBtn.parentElement.id;
-                    taskArr.forEach((el, i) => {
-                        if (el.taskKey === Number(parentLi)) {
-                            indexDelete = i;
-                        }
-                    });
-                    taskArr.splice(indexDelete, 1);
-                    delBtn.parentElement.parentElement.removeChild(delBtn.parentElement);
-                    refreshCounter();
-                })
-            })
-
-            //status ukończenia zadania
-            const completeButton = taskListUl.querySelectorAll('.completeButton');
-            completeButton.forEach(compBtn => {
-                compBtn.addEventListener('click', () => {
-                    let indexComp;
-                    const parentLi = compBtn.parentElement.id;
-                    taskArr.forEach((el, i) => {
-                        if (el.taskKey === Number(parentLi)) {
-                            indexComp = i;
-                        }
-                    });
-
-                    if (compBtn.parentElement.className.indexOf("completed") === -1) {
-                        compBtn.parentElement.classList.add('completed');
-                        compBtn.innerText = `not ready`;
-                        taskArr[indexComp].taskDone = true;
-                    } else {
-                        compBtn.parentElement.classList.remove('completed');
-                        compBtn.innerText = `complited`;
-                        taskArr[indexComp].taskDone = false;
-                    }
-                    refreshCounter();
-                })
-            })
-
-            //usuwanie zrobionych zadań
-            const removeFinished = document.getElementById('infoOut-removeFinishedTasksButton');
-            removeFinished.style.display = "block";
-            removeFinished.addEventListener('click', () => {
-                const completedTasks = document.querySelectorAll('.completed');
-                completedTasks.forEach(finishedLi => {
-                    finishedLi.parentElement.removeChild(finishedLi);
-                })
-            })
         }
+    })
+    taskListUl.addEventListener('click', (e)=>{
+        if (e.target.classList.contains("deleteButton")){ //usuwanie zadania                                let indexDelete;
+            const parentLi = e.target.parentElement.id;
+            let indexDelete;
+            taskArr.forEach((el, i) => {
+                if (el.taskKey === Number(parentLi)) {indexDelete = i}
+            });
+            taskArr.splice(indexDelete, 1);
+            e.target.parentElement.parentElement.removeChild(e.target.parentElement);
+            refreshCounter();
+            refreshList(taskArr);
+        }
+
+        // //status ukończenia zadania
+        // const completeButton = taskListUl.querySelectorAll('.completeButton');
+        // completeButton.forEach(compBtn => {
+        //     compBtn.addEventListener('click', () => {
+        //         let indexComp;
+        //         const parentLi = compBtn.parentElement.id;
+        //         taskArr.forEach((el, i) => {
+        //             if (el.taskKey === Number(parentLi)) {
+        //                 indexComp = i;
+        //             }
+        //         });
+        //
+        //         if (compBtn.parentElement.className.indexOf("completed") === -1) {
+        //             compBtn.parentElement.classList.add('completed');
+        //             compBtn.innerText = `not ready`;
+        //             taskArr[indexComp].taskDone = true;
+        //         } else {
+        //             compBtn.parentElement.classList.remove('completed');
+        //             compBtn.innerText = `complited`;
+        //             taskArr[indexComp].taskDone = false;
+        //         }
+        //         refreshCounter();
+        //     })
+        // })
+        //
+        // //usuwanie zrobionych zadań
+        // const removeFinished = document.getElementById('infoOut-removeFinishedTasksButton');
+        // removeFinished.style.display = "block";
+        // removeFinished.addEventListener('click', () => {
+        //     const completedTasks = document.querySelectorAll('.completed');
+        //     completedTasks.forEach(finishedLi => {
+        //         finishedLi.parentElement.removeChild(finishedLi);
+        //     })
+        // })
     })
 })
